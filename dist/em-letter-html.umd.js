59919973753917f523f5d07c63e00336384221bc
yuda-lyu <firsemisphere@gmail.com>
2019-08-27 18:45:55 +0800

    update: pks

--- dist/em-letter-html.umd.js
+++ dist/em-letter-html.umd.js
@@ -1,5 +1,5 @@
 /*!
- * em-letter-html v1.0.6
+ * em-letter-html v1.0.7
  * (c) 2018-2019 yuda-lyu(semisphere)
  * Released under the MIT License.
  */(function(a,b){"object"==typeof exports&&"undefined"!=typeof module?module.exports=b():"function"==typeof define&&define.amd?define(b):(a=a||self,a["em-letter-html"]=b())})(this,function(){'use strict';return"<table style=\"width:100%;\">\n<tbody>\n\n<tr style=\"display:none;\">\n    <td>\n        {emMessage}\n    </td>\n</tr>\n\n<tr>\n    <td style=\"padding:30px; font-family:'Microsoft JhengHei'; background-color:#666; text-align:center;\">\n        <a style=\"color:#aaffff; font-size:24pt; text-decoration:none;\" href=\"{emWebUrl}\" target=\"_blank\">{emWebName}</a>\n        <p style=\"margin:10px 0px 0px 0px; color:#eee; font-size:12pt;\">\n            {emWebDescription}\n        </p>\n    </td>\n</tr>\n\n<tr>\n    <td style=\"padding:20px; font-family:'Microsoft JhengHei';\">\n        <p style=\"\">{emMessage}</p>\n    </td>\n</tr>\n\n<tr>\n    <td style=\"padding:20px; font-family:'Microsoft JhengHei'; background-color:#f5f5f5; text-align:center;\">\n        <p style=\"color:#f26; font-size:11pt;\">\u3010{emLetterDoNotReplayMessage}\u3011</p>\n        <p style=\"\">{emLetterTeamMessage}</p>\n        <p style=\"padding:1em 0px 0px 0px; color:#aaa;\">{emLetterLinks}</p>\n    </td>\n</tr>\n\n</tbody>\n</table>"});
--- dist/em-sign-up-html.umd.js
+++ dist/em-sign-up-html.umd.js
@@ -1,5 +1,5 @@
 /*!
- * em-sign-up-html v1.0.6
+ * em-sign-up-html v1.0.7
  * (c) 2018-2019 yuda-lyu(semisphere)
  * Released under the MIT License.
  */(function(a,b){"object"==typeof exports&&"undefined"!=typeof module?module.exports=b():"function"==typeof define&&define.amd?define(b):(a=a||self,a["em-sign-up-html"]=b())})(this,function(){'use strict';return"<p>Dear {name} :</p>\n\n<p>Your registration request has been received. Please click the link below to verify your account.</p>\n\n<p><a href=\"{urlCheckCode}\" target=\"_blank\">Confirm Account</a></p>"});
