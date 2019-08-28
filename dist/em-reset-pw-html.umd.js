7051a7ab7cc9fa265f40ac621cf705d87ca34006
yuda-lyu <firsemisphere@gmail.com>
2019-08-28 18:17:43 +0800

    update: pks

--- dist/em-reset-pw-html.umd.js
+++ dist/em-reset-pw-html.umd.js
@@ -1,5 +1,5 @@
 /*!
- * em-reset-pw-html v1.0.7
+ * em-reset-pw-html v1.0.8
  * (c) 2018-2019 yuda-lyu(semisphere)
  * Released under the MIT License.
  */(function(a,b){"object"==typeof exports&&"undefined"!=typeof module?module.exports=b():"function"==typeof define&&define.amd?define(b):(a=a||self,a["em-reset-pw-html"]=b())})(this,function(){'use strict';return"<p>Dear {name} :</p>\n\n<p>You have received your change password request. Please click the link below to verify your account.</p>\n\n<p><a href=\"{urlCheckCode}\" target=\"_blank\">Confirm Account</a></p>"});
